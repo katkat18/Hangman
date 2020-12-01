e2259803d2e06af6ffe8d04d0d707b5b7fb3419d
katrinajamir <katrinajamir@gmail.com>
2020-12-01 04:42:56 -0800

    most styling complete

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -6,10 +6,10 @@ const $wordSlotOutput = $('#word-container');
 let wordSlotArray = []; 
 
 //other variables:
-let word = "zoro";
+let word = "katrina";
 let wordArray = []; 
 let guessedLetters = []; 
-let hint = "A very tiny dog";
+let hint = "A girl that is simmering";
 let letter = "";
 let guesses = 0; 
 const guessLimit = 6;
@@ -21,12 +21,13 @@ let currentImageNumber = 1;
 //generating random words and hints (definitions) using Wordnik API
 const apiKey = '1iq9ncc7hnq0r1io7gzrevu7wkfr7si0c2uwh9tetrtelhn2d';
 const fetchWordURL = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${apiKey}`;
-//const fetchHintURL = `https://api.wordnik.com/v4/word.json/${currWord}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`;
+const fetchHintURL = `https://api.wordnik.com/v4/word.json/${word}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`;
 
 //hide win/lose pop-ups
+/*
 $('#win-pop-up').hide();
 $('#lose-pop-up').hide();
-
+*/
 //objects
 
 class Game{
@@ -61,23 +62,22 @@ class Game{
         //return true or false 
 
     }
+    //testing purposes
     newGame(){
-        //fetch new word and hint -- store it 
-        //fetchWord(fetchWordURL);
-        //enable all buttons
+        //console.log(guesses);
         $('.letter').removeAttr('disabled');
         //set up game lines 
         wordArray = word.split("");
-        
+    
         //set up letter slots
-
+        
         for(let i = 0; i < wordArray.length; i++){
-            console.log("splitting array ");
-            console.log(`split array: ${wordArray[i]}`);
+        console.log("splitting array ");
+        console.log(`split array: ${wordArray[i]}`);
         }
         //make lines on HTML output word 
         for(let i = 0; i < wordArray.length; i++){
-            wordSlotArray.push(`___ `);    
+            wordSlotArray.push(`<p>_</p>`);    
         }
         $wordSlotOutput.html(wordSlotArray);
         //set up all variables needed for word and hint
@@ -88,6 +88,44 @@ class Game{
         resetImage(); 
 
     }
+
+    //this works but not consistently 
+    /*
+    newGame(){
+        //fetch new word and hint -- store it 
+        //fetchWord(fetchWordURL);
+        //enable all buttons
+        
+        
+        fetchWordHint().then(function(){
+            //console.log(guesses);
+            $('.letter').removeAttr('disabled');
+            //set up game lines 
+            wordArray = word.split("");
+        
+            //set up letter slots
+            
+            for(let i = 0; i < wordArray.length; i++){
+            console.log("splitting array ");
+            console.log(`split array: ${wordArray[i]}`);
+            }
+            //make lines on HTML output word 
+            for(let i = 0; i < wordArray.length; i++){
+                wordSlotArray.push(`___ `);    
+            }
+            $wordSlotOutput.html(wordSlotArray);
+            //set up all variables needed for word and hint
+            //display hint 
+            $('#hint-output').html(`Hint: ${hint}`); 
+            //display guess output 
+            updateGuessOutput(guesses);
+            resetImage(); 
+            }).catch(function(e){
+            console.log(`error in newGame(): ${e}`);
+            });
+        
+    }
+    */
 }
 //start a new game 
 const game = new Game(); 
@@ -97,33 +135,28 @@ $('#play-btn').click(function(){
 
     //start a new game
     console.log("starting a new game");
+    //fetchWordHint(); 
     game.newGame(); 
     //fetchWord(fetchWordURL);
     //put word into an array 
 
     /*
     wordArray = word.split(""); 
-
     for(let i = 0; i < wordArray.length; i++){
         console.log("splitting array ");
         console.log(`split array: ${wordArray[i]}`);
     }
-
     //make lines on HTML output word 
     for(let i = 0; i < wordArray.length; i++){
         wordSlotArray.push(`___ `);
         //$wordSlotOutput.append(`___ `);
     
     }
-
     $wordSlotOutput.html(wordSlotArray);
-
     //display hint
     $('#hint-output').html(`Hint: ${hint}`);
-
     //display output 
     updateGuessOutput(guesses);
-
 */
 
 });
@@ -150,7 +183,7 @@ $('.letter').click(function(){
     for(let i = 0; i < wordArray.length; i++){
         //correct
         if(letter == wordArray[i]){
-            wordSlotArray[i] = `${letter}            `; 
+            wordSlotArray[i] = `<p>${letter}</p>`; 
             $wordSlotOutput.html(wordSlotArray);
             correctGuess = true; 
         }
@@ -172,10 +205,46 @@ $('.letter').click(function(){
     $(this).attr('disabled', 'disabled');
 
 });
+//works but not consistently 
+/*
+async function fetchWordHint(){
+    word = await fetch(fetchWordURL)
+                    .then(function(res){
+                        console.log(`word res: ${res}`);
+                        if(res.ok){
+                            //do something
+                            return res.json(); 
+                         }
+                    })
+                    .then(function(data){
+                        console.log(`word data: ${data.word}`);
+                        return data.word; 
+    
+                    })
+                    .catch(function(err){
+                        console.log(`error on fetchWord: ${err}`);
+                    });
+    console.log(`new word: ${word}`);
+    hint = await fetch(`https://api.wordnik.com/v4/word.json/${word}/definitions?limit=3&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`)
+                    .then(function(res){
+                        console.log(`hint res: ${res}`);
+                        if(res.ok){
+                            return res.json();
+                        }
+                    })
+                    .then(function(data){
+                        console.log(`hint data: ${data[2].text}`);
+                        return data[2].text; 
+                    })
+                    .catch(function(err){
+                        console.log(`error fetchHint: ${err}`)
+                    });
+    console.log(`new hint: ${hint}`); 
+}
 
-
+*/
 //functions 
-
+/*
 //return a random word 
 function fetchWord(url){
     fetch(url)
@@ -213,7 +282,7 @@ function fetchHint(url){
             console.log(`error fetchHint: ${err}`)
         });
 }
-
+*/
 
 function updateGuessOutput(num){
     $('#guess-output').html(`Incorrect guesses: ${num}/6`);
@@ -228,9 +297,7 @@ function checkForWin(){
         console.log("you lose");
         //$('#lose-pop-up').css('display', 'block');
         $('#lose-pop-up').css('opacity', 1);
-
     }
-
  
     //check to see if word has been guessed 
     for(let i = 0; i < wordArray.length; i++){
@@ -238,11 +305,9 @@ function checkForWin(){
             correctLetters++;
         }
     }
-
     if(correctLetters == wordArray.length){
         console.log("you win!");
     }
-
 }
 */
 function updateImage(){
